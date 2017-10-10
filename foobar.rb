839ba904e98e760c4a11c263f2e132136da7ee21
Jonathan Chu <jonmchu@berkeley.edu>
2017-10-09 17:25:44 -0700

    pushing up solutions

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,11 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    ints = a.uniq.map{|val| val.to_i}.select{|val| val.even? and val<10}
+
+    # For information about inject function read
+    # https://codequizzes.wordpress.com/2013/07/16/rubys-inject-method/
+    ints.inject(0){|sum,x| sum + (2+x)}
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort.map{|val| val+=1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
